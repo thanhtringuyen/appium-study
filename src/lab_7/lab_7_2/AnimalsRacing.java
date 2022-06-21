c39a0782b3abba40d8b7d4c5a89d1ca478c18738
thanhtringuyen <62368688+thanhtringuyen@users.noreply.github.com>
2022-06-21 10:48:41 +0700

    Update logic

--- src/lab_7/lab_7_2/AnimalsRacing.java
+++ src/lab_7/lab_7_2/AnimalsRacing.java
@@ -7,35 +7,38 @@ public class AnimalsRacing {
         Animal dog = new Dog();
         Animal horse = new Horse();
         Animal tiger = new Tiger();
-        Animal lion = new Lion();
-        Animal cheetah = new Cheetah();
+        Animal dragon = new Lion();
+        Animal leopard = new Cheetah();
         ArrayList<Animal> animalsList = new ArrayList<Animal>();
 
         animalsList.add(horse);
         animalsList.add(tiger);
         animalsList.add(dog);
-        animalsList.add(lion);
-        animalsList.add(cheetah);
+        animalsList.add(dragon);
+        animalsList.add(leopard);
         AnimalsRacing(animalsList);
     }
 
-    public static void AnimalsRacing(ArrayList<Animal> animalList){
-        Animal winner =  animalList.get(0);
-        ArrayList<Animal> winner = new ArrayList<>();
-        winner.add(winner);
 
-        for (int i=1; i<animalList.size();i++) {
+    public static void AnimalsRacing(ArrayList<Animal> animalList) {
+        Animal winner = animalList.get(0);
+        ArrayList<Animal> winners = new ArrayList<>();
+        winners.add(winner);
+
+        for (int i = 1; i < animalList.size(); i++) {
             if (animalList.get(i).speed() > winner.speed()) {
                 winner = animalList.get(i);
-                winner.clear();
-                winner.add(winner);
+                winners.clear();
+                winners.add(winner);
+            } else if (animalList.get(i).speed() == winner.speed()) {
+                winner = animalList.get(i);
+                winners.add(winner);
             }
         }
-        if (winner.size()>1)
-            System.out.printf("there are %d winners:", winner.size());
-        for (Animal i: winner){
-            System.out.printf("The winner is <%s> with amazing speed at: <%s>\n" , i.getAnimalType(),  i.getSpeed() + " km/h");
+            if (winners.size() > 1)
+                System.out.printf("there are %d winners:\n", winners.size());
+            for (Animal animal : winners) {
+                System.out.printf("The winner is <%s> with amazing speed at: <%s>\n", animal.getAnimalType(), animal.getSpeed() + " km/h");
+            }
         }
-
     }
-}
