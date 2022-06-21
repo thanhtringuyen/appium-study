898f3a43f7bff39cabe4967476592d02813c1b59
thanhtringuyen <62368688+thanhtringuyen@users.noreply.github.com>
2022-06-21 12:18:06 +0700

    Update better logic

--- src/lab_8/Animal_Racing.java
+++ src/lab_8/Animal_Racing.java
@@ -5,43 +5,56 @@ import java.util.ArrayList;
 
 public class Animal_Racing {
 
-    public static void checkAnimalWinner(ArrayList<AnimalBuilder> animalList){
-        AnimalBuilder winner = new AnimalBuilder.Builder().withAnimalType("winner").withSpeed(0).withWings(false).animalbuilder();
-        ArrayList<AnimalBuilder> racingAnimals = new ArrayList<>();
-        racingAnimals.add(winner);
-        for (int i=1; i<animalList.size(); i++) {
-            if (animalList.get(i).withWings())
-                continue;
-            if (animalList.get(i).getSpeed() > winner.getSpeed()) {
-                winner = animalList.get(i);
-                racingAnimals.clear();
-                racingAnimals.add(winner);
+    public static void checkAnimalWinner(ArrayList<Animal> animalList){
+        ArrayList<Animal> unFlyable = new ArrayList<>();
+        for (Animal animal : animalList) {
+            if (!animal.withWings()) {
+                unFlyable.add(animal);
             }
         }
-        if(racingAnimals.size()==1){
-            if(racingAnimals.get(0).getName().equals("winner"))
-                System.out.println("All animals can fly, so we can not see which is the winner");
-            else
-                System.out.println("The winner is %s with amazing speed at: %s km/h", racingAnimals.get(0).getName(), racingAnimals.get(0).getSpeed());
-            return;
+        ArrayList<Animal> racingAnimals = new ArrayList<>();
+        if (unFlyable.size() > 0) {
+            Animal winner = unFlyable.get(0);
+            racingAnimals.add(winner);
+            for (int i = 1; i< unFlyable.size(); i++) {
+                if (winner != null && unFlyable.get(i).getSpeed() > winner.getSpeed()) {
+                    winner = unFlyable.get(i);
+                    racingAnimals.clear();
+                    racingAnimals.add(winner);
+                } else if (winner != null &&  unFlyable.get(i).getSpeed() == winner.getSpeed()){
+                    winner = unFlyable.get(i);
+                    racingAnimals.add(winner);
+                }
+            }
         }
+        if (unFlyable.size() == 0) {
+            System.out.println("All animals can fly, so we can not see which is the winner");
+        } else if (racingAnimals.size() > 0){
+            System.out.printf("There are %d winners:\n", racingAnimals.size());
+            for (Animal animal : racingAnimals) {
+                System.out.printf("The winner is %s with amazing speed at: %s km/h\n",
+                        animal.getName(),
+                        animal.getSpeed());
+            }
+        }
+
     }
 
     public static void main(String[] args) {
-        AnimalBuilder eagle = new AnimalBuilder.Builder().withAnimalType("Eagle").withSpeed(new SecureRandom().nextInt(101))
-                .withWings(true).animalbuilder();
-        AnimalBuilder hummingbird = new AnimalBuilder.Builder().withAnimalType("Hummingbird").withSpeed(new SecureRandom().nextInt(71))
-                .withWings(true).animalbuilder();
-        AnimalBuilder falcon = new AnimalBuilder.Builder().withAnimalType("Falcon").withSpeed(new SecureRandom().nextInt(91))
-                .withWings(true).animalbuilder();
-        AnimalBuilder tiger = new AnimalBuilder.Builder().withAnimalType("Tiger").withSpeed(new SecureRandom().nextInt(81))
-                .withWings(false).animalbuilder();
-        AnimalBuilder snake = new AnimalBuilder.Builder().withAnimalType("Snake").withSpeed(new SecureRandom().nextInt(31))
-                .withWings(false).animalbuilder();
-        AnimalBuilder pikachu = new AnimalBuilder.Builder().withAnimalType("Pikachu").withSpeed(new SecureRandom().nextInt(101))
-                .withWings(false).animalbuilder();
+        Animal eagle = new Animal.Builder().withName("Eagle").withSpeed(new SecureRandom().nextInt(101))
+                .withWings(true).build();
+        Animal hummingbird = new Animal.Builder().withName("Hummingbird").withSpeed(new SecureRandom().nextInt(71))
+                .withWings(true).build();
+        Animal falcon = new Animal.Builder().withName("Falcon").withSpeed(new SecureRandom().nextInt(91))
+                .withWings(true).build();
+        Animal tiger = new Animal.Builder().withName("Tiger").withSpeed(new SecureRandom().nextInt(1))
+                .withWings(false).build();
+        Animal snake = new Animal.Builder().withName("Snake").withSpeed(new SecureRandom().nextInt(1))
+                .withWings(false).build();
+        Animal pikachu = new Animal.Builder().withName("Pikachu").withSpeed(new SecureRandom().nextInt(101))
+                .withWings(false).build();
 
-        ArrayList<AnimalBuilder> animalList = new ArrayList<>();
+        ArrayList<Animal> animalList = new ArrayList<>();
         animalList.add(eagle);
         animalList.add(hummingbird);
         animalList.add(falcon);
